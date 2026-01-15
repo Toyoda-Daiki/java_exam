614c1fc068b449c52c55dbc32a0a982d2c0a9b9a
da1To <da.12i28.ki@gmail.com>
2026-01-15 10:07:59 +0900

    プログラミング入門1

---
+++ 1-1.test-java-entry1/src/Exam2.java
@@ -0,0 +1,8 @@
+public class Exam2 {
+  static String name = "豊田大樹";
+  public static void main(String[] args) {
+    System.out.println(name);
+    System.out.println(name);
+    System.out.println(name);
+  }
+}
---
+++ 1-1.test-java-entry1/src/Exam3.java
@@ -0,0 +1,10 @@
+public class Exam3 {
+  public static void main(String[] args) {
+    System.out.println( "小計");
+    System.out.println(200 * 3 + 250 * 4 + "円");
+    System.out.println( "消費税");
+    System.out.println((int)(200 * 3 * 0.1 + 250 * 4 * 0.1) + "円");
+    System.out.println( "合計金額");
+    System.out.println((int)(200 * 3 * 1.1 + 250 * 4 * 1.1) + "円");
+  }
+}
