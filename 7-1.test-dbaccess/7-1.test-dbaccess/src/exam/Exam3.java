bba0374e8f631a755b992ba3856721675a1eb08a
da1To <da.12i28.ki@gmail.com>
2026-01-28 10:37:19 +0900

    DBアクセス1

---
+++ 7-1.test-dbaccess/7-1.test-dbaccess/src/exam/Exam3.java
@@ -0,0 +1,63 @@
+package exam;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * 雛形クラスです.<br>
+ * これをコピーペーストして問題を解いて構いません。
+ * 
+ * @author igamasayuki
+ */
+public class Exam3 {
+	public static void main(String[] args) {
+		// 接続情報
+		String url = "jdbc:postgresql://localhost:5432/student";
+		String user = "postgres";
+		String password = "1228";
+
+		Connection con = null; // 使用する変数の宣言
+		PreparedStatement pstmt = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		try {
+			// (1)データベースに接続
+			con = DriverManager.getConnection(url, user, password);
+
+			// (2)SQL文を作成
+			sql = "SELECT m.id, m.name, m.age, d.name AS dep_name FROM test_members m JOIN test_deps d ON m.dep_id = d.id";
+
+			// (3)SQL実行準備
+			pstmt = con.prepareStatement(sql);
+
+			// (4)SQL実行
+			rs = pstmt.executeQuery();
+
+			// (5)結果の操作
+			while (rs.next()) {
+				int id = rs.getInt("m.id");
+				String name = rs.getString("m.name");
+				int age = rs.getInt("m.age");
+				String dep_name = rs.getString("d.dep_name");
+
+				System.out.print("id : " + id);
+				System.out.print("name :" + name);
+				System.out.print("age :" + age);
+				System.out.println("dep_name :" + dep_name);
+			}
+		} catch (SQLException ex) {
+			System.err.println("SQL = " + sql);
+			ex.printStackTrace();
+		} finally {
+			try {
+				// (6) メモリの解放(切断)
+				con.close();
+			} catch (Exception ex) {
+			}
+		}
+	}
+}
